bf0e7b5c415722b053d5b785b469775ea460d821
Arafat Akhter <arafatakhterforhad@gmail.com>
2022-10-11 13:17:58 +0600

    rechart added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Blog from "./components/Blog/Blog";
 import Error from "./components/Error/Error";
 import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
 import TopicDetails from "./components/TopicDetails/TopicDetails";
 import Main from "./layouts/Main";
 
@@ -20,6 +21,12 @@ function App() {
         {
           path: "/blog",
           element: <Blog></Blog>,
+        },
+        {
+          path: "/statistics",
+          loader: async () =>
+            fetch("https://openapi.programming-hero.com/api/quiz"),
+          element: <Statistics></Statistics>,
         },
         {
           path: "/quiz/:topicId",
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+
+const Statistics = () => {
+  const quizTopics = useLoaderData();
+  const data = quizTopics.data;
+
+  return (
+    <div className="flex justify-center mt-28">
+      <div className="z-[-1]">
+        <LineChart
+          width={700}
+          height={400}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+        >
+          <Line type="monotone" dataKey="total" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </LineChart>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
